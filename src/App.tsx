915f7627c89714e9046c5c8b6e97b47d4019adc2
marcelaTrujilloToro <marces.ttblue@gmail.com>
2021-06-10 10:07:13 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from '@material-ui/core/Button'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="contained" color="secondary">
+        Aceptar
+      </Button>
+    </div>
+  );
+}
+
+export default App;
